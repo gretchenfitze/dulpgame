0f91a4072c51375ef6f854aee39bfdd5c359a546
Margarita Klubochkina <gretchenfitze@gmail.com>
2016-09-01 00:14:31 +0300

    Fix addtohomescreen lib

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,10 @@
 import Game from './components/Game.js';
 import viewportUnitsBuggyfill from 'viewport-units-buggyfill';
-import './../css/addtohomescreen.css';
 import levels from './../data/levels.json';
+import 'add-to-homescreen/addtohomescreen.js';
+import 'add-to-homescreen/dist/style/addtohomescreen.css';
+
+window.addToHomescreen();
 
 if (window.navigator.userAgent.match(/iPad/i) || window.navigator.userAgent.match(/iPhone/i)) {
 	viewportUnitsBuggyfill.init();
